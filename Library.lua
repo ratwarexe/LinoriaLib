2dec720f578fe013075ecdd285d3406eca6fb964
ratwarexe <ratware.exe@atomicmail.io>
2025-08-13 21:49:49 -0400

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -31,7 +31,7 @@ local Library = {
 
     FontColor = Color3.fromRGB(255, 255, 255);
     MainColor = Color3.fromRGB(255, 255, 255);
-    BackgroundColor = Color3.fromRGB(255, 165, 0);
+    BackgroundColor = Color3.fromRGB(255, 130, 0);
     AccentColor = Color3.fromRGB(0, 0, 0);
     OutlineColor = Color3.fromRGB(0, 0, 0);
     RiskColor = Color3.fromRGB(255, 50, 50),
